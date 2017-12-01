2dde025177eec180cb494c385f3c7055606278da
Yesenia Narvaez <yesenian1992@gmail.com>
2017-12-01 01:52:25 +0000

    minimum met

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 /*global $ APIKEY*/
 $(document).ready(function() {
     $.ajax({
-    method: "GET",
-    url: "https://newsapi.org/v2/sources",
-    data: { category:"business", country: "us", language: "en", apikey: APIKEY},
-    success: function(data){
-    if (data.status == "ok") {
-    for (var i = 0; i < data.sources.length; i++) {
-   var source = document.createElement("OPTION"); 
-   source.setAttribute("value", data.sources[i].id)
-   source.innerHTML = data.sources[i].name;
-   document.getElementById('selection').appendChild(source);
-    }
-
- }
-    }
- 
-
+        method: "GET",
+        url: "https://newsapi.org/v2/sources",
+        data: { category:"business", country: "us", language: "en", apikey: APIKEY},
+        success: function(data){
+            if (data.status == "ok") {
+                for (var i = 0; i < data.sources.length; i++) {
+                   var source = document.createElement("OPTION"); 
+                   source.setAttribute("value", data.sources[i].id)
+                   source.innerHTML = data.sources[i].name;
+                   document.getElementById('selection').appendChild(source);
+                }
+            }
+        }
     })
    
-$('#source').submit(function(event) {
-    event.preventDefault();
-    alert(document.getElementById("selection").value)
-    });
+    $('#source').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            method: "GET",
+            url: "https://newsapi.org/v2/top-headlines",
+            data: { sources: document.getElementById('selection').value, category:"business", country: "us", language: "en", apikey: APIKEY},
+            success: function(data2){
+                if (data2.status == "ok") {
+                    for (var i = 0; i < data2.articles.length; i++) {
+                       var source = document.createElement("P"); 
+                       source.innerHTML = data2.articles[i].title;
+                       document.getElementById('display').appendChild(source);
+                    }
+                }
+            }
+        })
+    })
 })
